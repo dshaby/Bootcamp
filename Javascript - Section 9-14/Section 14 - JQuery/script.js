6800bf27cf01891508783b86799ce3e9fbd85174
dshaby <daniel@businessesoftheearth.org>
2022-01-24 14:56:54 -0800

    JQuery

---
+++ Javascript - Section 9-14/Section 14 - JQuery/script.js
@@ -0,0 +1,16 @@
+// $("h1").css("color", "red");
+$("h1.title");
+$("#headerSectionID h1");
+
+$("button").css("color", "green");
+
+$("h1").addClass("big-title margin-50");
+
+$("h1").html("<strong>Heyyyyy</strong>");
+
+$("button").text("Change my text :)");
+
+$("img").attr("src");
+
+
+$("a").attr("href", "https://www.yahoo.com");
