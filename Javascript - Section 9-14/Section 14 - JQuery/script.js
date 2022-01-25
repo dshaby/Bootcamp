4d9f21c659cd41910748831ed94add357dea4370
dshaby <daniel@businessesoftheearth.org>
2022-01-25 12:54:47 -0800

    Finishing JQuery

--- Javascript - Section 9-14/Section 14 - JQuery/script.js
+++ Javascript - Section 9-14/Section 14 - JQuery/script.js
@@ -14,3 +14,52 @@ $("img").attr("src");
 
 
 $("a").attr("href", "https://www.yahoo.com");
+
+// Event Listeners - 1 Element
+$("h1").click(function () {
+    $("h1").css("color", "purple");
+})
+// instead of
+// document.querySelector("h1").addEventListener("click", function() {...})
+
+// Event Listeners - Many elements:
+$("button").click(function () {
+    $("h1").css("color", "green");
+})
+// Instead of:
+// for (var i = 0; i < 5; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function () {
+//         document.querySelector("h1").style.color = "purple";
+//     });
+// }
+
+$("input").keydown(function (event) {
+    console.log(event.key);
+});
+
+$("body").keydown(function (event) {
+    $("h1").text(event.key);
+})
+
+$("h1").on("mouseover", function () {
+    $("h1").css("color", "aquamarine");
+});
+
+$("h1").before("<button>New</button>");
+
+// $("button").on("click", function () {
+//     $("h1").slideToggle();
+// })
+
+// $("button").on("click", function () {
+//     $("h1").animate({
+//         opacity: 0.5
+//     });
+// })
+
+// CHAINING Methods
+$("button").on("click", function () {
+    $("h1").slideUp().slideDown().animate({
+        opacity: 0.5
+    });
+});
