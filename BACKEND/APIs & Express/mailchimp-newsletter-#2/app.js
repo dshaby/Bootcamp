3fd9fc566005d907ba286098dcb59e2f4f5e1c42
dshaby <daniel@businessesoftheearth.org>
2022-03-08 19:34:48 -0800

    Failure Page using EJS & using correct "PUT" method
    Since we're updating a list, "PUT" method is required with setListMember, not addListMember.  A new module had to be addeed (methodOverride)

--- BACKEND/APIs & Express/mailchimp-newsletter-#2/app.js
+++ BACKEND/APIs & Express/mailchimp-newsletter-#2/app.js
@@ -1,23 +1,27 @@
 const express = require('express');
+const methodOverride = require('method-override');
 const app = express();
+app.use(methodOverride('_method'));
+
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+// const connect = require('connect');
 app.use(express.urlencoded({ extended: true }));
 
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 mailchimp.setConfig({
-    apiKey: "37eee9e90d855211c79a7892792252bd-us14",
+    apiKey: "6ca38de478a099a6b698fcca203b2acf-us14",
     server: "us14",
 });
-// API: 
+// API: f
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.post('/', (req, res) => {
+app.put('/', (req, res) => {
     const subscriber = {
         fName: req.body.firstName,
         lName: req.body.lastName,
@@ -25,8 +29,7 @@ app.post('/', (req, res) => {
     }
     const run = async () => {
         try {
-            const response = await mailchimp.lists.addListMember("57667409b4", {
-                email_address: subscriber.email,
+            const response = await mailchimp.lists.setListMember("57667409b4", subscriber.email, {
                 merge_fields: {
                     FNAME: subscriber.fName,
                     LNAME: subscriber.lName,
@@ -34,32 +37,18 @@ app.post('/', (req, res) => {
                 status: "subscribed",
             });
             console.log(response);
-            res.redirect('/success');
+            res.render('success');
         }
         catch (error) {
             console.log(error);
-            res.redirect('/failure');
+            res.render('failure', {
+                ejsErrorTitle: error.response.body.title,
+                ejsErrorDetail: error.response.body.detail,
+                ejsErrorStatus: error.status,
+            });
         }
     }
     run();
 });
 
-app.get('/success', (req, res) => {
-    res.render('success');
-});
-
-app.get('/failure', (req, res) => {
-    res.render('failure');
-})
-
-app.get('*', function (req, res) {
-    res.status(401).render('failure');
-});
-
-app.get('*', function (req, res) {
-    res.status(404).render('failure');
-});
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-// FIGURE out how to handle errors....
