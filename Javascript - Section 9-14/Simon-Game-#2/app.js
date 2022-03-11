c8ed37fe2f46c064b639ae3e1c5887e66594c32d
dshaby <daniel@businessesoftheearth.org>
2022-03-11 14:46:00 -0800

    Making progress on Simon Game

--- Javascript - Section 9-14/Simon-Game-#2/app.js
+++ Javascript - Section 9-14/Simon-Game-#2/app.js
@@ -1,7 +1,7 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 
 const gamePattern = [];
-const userClickedPattern = [];
+let userClickedPattern = [];
 
 $("body").one("keypress", function () {
     nextSequence();
@@ -42,7 +42,55 @@ function buttonClick() {
         animate("#" + userChosenColor);
 
         console.log("userClickedPattern: " + userClickedPattern);
-        // checkAnswer(); (not yet created)
+        checkAnswer();
     });
 };
 buttonClick();
+
+function checkAnswer() {
+
+    for (let i = 0; i < userClickedPattern.length; i++) {
+
+        if (userClickedPattern[i] === gamePattern[i]) {
+            console.log("Both arrays are equal up until [i]");
+
+            if (gamePattern.length === userClickedPattern.length) {
+                setTimeout(function () {
+                    nextSequence();
+                }, 800);
+
+                userClickedPattern = [];
+            } else {
+                // FOR SOME REASON, I'M GETTING THAT gamePattern[1] === userClickedPattern[1] even if they're not
+                // although, gamePattern[0] appropriately checks if it's === to userClickedPattern[0]
+                // FIGURE THIS OUT
+            }
+
+        } else {
+            console.log("Not equal arrays");
+            stopGame();
+        }
+
+        // var is_same = gamePattern.every(function (color, index) {
+        //     return color === userClickedPattern[index]
+        // });
+        // console.log(is_same);
+        // if (is_same === true) {
+        // console.log("Both arrays are equal");
+        // setTimeout(function () {
+        //     nextSequence();
+        // }, 800);
+
+        // userClickedPattern = [];
+
+        // } else {
+        // console.log("Not equal arrays");
+        // stopGame();
+        // }
+    }
+}
+
+function stopGame() {
+    $("body").addClass("game-over").attr('id', 'wrong')
+    playSound("#wrong");
+}
