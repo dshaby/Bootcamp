3b09966805d09d42b829c3432490a0fc28c2cf1a
dshaby <88353394+dshaby@users.noreply.github.com>
2022-03-23 17:18:54 -0700

    Updating todo list

--- BACKEND/Databases/MongoDB/to-do-list-#2 copie/app.js
+++ BACKEND/Databases/MongoDB/to-do-list-#2 copie/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const { default: mongoose } = require('mongoose');
 const app = express()
 const port = 3000
@@ -9,8 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const dates = require('./dates');
 
-// const regularToDoList = ["Buy Food", "Cook Food", "EAT IT!!!"];
-// const workToDolist = ['Work List'];
+const workToDolist = ['Work List'];
 
 // Creating mongoose DB
 mongoose.connect("mongodb://localhost:27017/todolistDB");
@@ -39,7 +39,7 @@ const eatFood = new Item({
 });
 
 const defaultItems = [buyFood, cookFood, eatFood];
-
+// HOME ROUTE
 app.get('/', (req, res) => {
     Item.find({}, function (err, foundItems) {
         if (err) { console.log(err); }
@@ -56,19 +56,114 @@ app.get('/', (req, res) => {
             ejsTodaysDate: dates.todaysDate(),
             ejsWeekend: dates.weekend(),
             ejsToDoList: foundItems,
+            ejsDeleteRoute: "/delete",
             ejsRoute: "/"
         });
     });
 });
-
+// ADDING TO DB
 app.post('/', (req, res) => {
 
     const newItem = new Item({
         name: req.body.newItem
     });
 
-    newItem.save();
-    res.redirect('/');
+    newItem.save(function () {
+        console.log("Added " + newItem + " to the collection");
+        res.redirect('/');
+    });
+});
+
+// DELETING FROM DB
+app.post("/delete", (req, res) => {
+    checkedItem = req.body.checkedItem;
+
+    Item.findByIdAndRemove(checkedItem, function (err, foundItems) {
+        if (err) { console.log(err); }
+        else {
+            console.log(checkedItem);
+            console.log("Successfully deleted " + checkedItem);
+            res.redirect("/");
+        }
+    });
+});
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+function createDynamicModel(DynamicListModel, dynamicListCollection) {
+    DynamicListModel = mongoose.model(dynamicListCollection, toDoListSchema);
+
+    // Create documents
+    const firstItem = new DynamicListModel({
+        name: dynamicListCollection
+    });
+    const secondItem = new DynamicListModel({
+        name: "Second " + dynamicListCollection + " item"
+    });
+
+    const defaultDynamicItems = [firstItem, secondItem];
+
+    addDynamicItems(DynamicListModel, defaultDynamicItems);
+
+}
+
+function addDynamicItems(DynamicListModel, defaultDynamicItems, urlDynamicList) {
+    DynamicListModel.find({}, function (err, foundItems) {
+        if (err) { console.log(err); }
+        else if (foundItems.length === 0) {
+            DynamicListModel.insertMany(defaultDynamicItems, function (err) {
+                if (err) { console.log(err); }
+                else { console.log("Successfully inserted defaultDynamicItems " + defaultDynamicItems); }
+            });
+            foundItems = defaultDynamicItems;
+        }
+    });
+
+    loadDynamicPage(DynamicListModel, urlDynamicList);
+
+}
+
+function loadDynamicPage(DynamicListModel, urlDynamicList) {
+    DynamicListModel.find({}, function (err, foundItems) {
+        if (err) { console.log(err); }
+        else {
+            console.log("found the items Properly");
+            console.log(foundItems);
+            // res.render("index", {
+            //     ejsListType: DynamicListModel + " List",
+            //     ejsWeekend: dates.weekend(),
+            //     ejsTodaysDate: dates.todaysDate(),
+            //     ejsToDoList: foundItems,
+            // ejsDeleteRoute: "/:" + urlDynamicList,
+            // ejsRoute: "/" + urlDynamicList
+            // })
+        }
+    })
+    res.redirect("/");
+}
+
+app.get("/:listName", (req, res) => {
+    const urlDynamicList = req.params.listName;
+    const DynamicListModel = capitalizeFirstLetter(urlDynamicList);
+    const dynamicListCollection = capitalizeFirstLetter(urlDynamicList);
+
+    createDynamicModel(DynamicListModel, dynamicListCollection, urlDynamicList);
+});
+
+app.post("/:listName", (req, res) => {
+    checkedItem = req.body.checkedItem;
+    //same DynamicList??
+    DynamicList.findByIdAndRemove(checkedItem, function (err) {
+        if (err) { console.log(err); }
+        else {
+            console.log(checkedItem);
+            console.log("Successfully deleted " + checkedItem);
+            res.redirect("/" + dynamicList);
+            // Looks fine here too.
+        }
+    });
 })
 
 app.get('/work', (req, res) => {
