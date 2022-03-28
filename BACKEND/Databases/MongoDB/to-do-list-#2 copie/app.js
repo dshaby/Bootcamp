84abb53e0aedbb75e24dcccde6b347df7e6479f3
dshaby <daniel@businessesoftheearth.org>
2022-03-28 08:01:16 -0700

    Updating todolist#2

--- BACKEND/Databases/MongoDB/to-do-list-#2 copie/app.js
+++ BACKEND/Databases/MongoDB/to-do-list-#2 copie/app.js
@@ -10,12 +10,10 @@ app.use(express.urlencoded({ extended: true }));
 
 const dates = require('./dates');
 
-const workToDolist = ['Work List'];
-
 // Creating mongoose DB
 mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-// Create Schema
+// Create Schemas
 const toDoListSchema = {
     name: {
         type: String,
@@ -23,162 +21,218 @@ const toDoListSchema = {
     }
 };
 
-// Create model
+const dynamicListSchema = {
+    dynamicListName: {
+        type: String,
+        required: [false, "You need a name for your list"]
+    },
+    items: [toDoListSchema] // has same schema as traditional to-do-list
+}
+
+// Create models
 const Item = mongoose.model("Item", toDoListSchema);
-// Create a document
-const buyFood = new Item({
-    name: "Buy Food"
-});
+const DynamicList = mongoose.model("DynamicList", dynamicListSchema);
 
-const cookFood = new Item({
-    name: "Cook Food"
-});
+// Create & Save Default Docs
+const listName = "general to-do"
+function createDocs(listName) {
+    const firstItem = new Item({
+        name: "First " + listName + " item"
+    });
 
-const eatFood = new Item({
-    name: "Eat Food"
-});
+    const secondItem = new Item({
+        name: "Second " + listName + " item"
+    });
 
-const defaultItems = [buyFood, cookFood, eatFood];
+    const thirdItem = new Item({
+        name: "Third " + listName + " item",
+    });
+
+    const defaultItems = [firstItem, secondItem, thirdItem];
+    console.log("Finished creating Docs, but not yet saved");
+
+    if (listName === "general to-do") {
+        Item.find({}, function (err, foundItems) {
+            if (foundItems.length === 0) {
+                Item.insertMany(defaultItems, function (err) {
+                    if (err) { console.log(err); }
+                    else {
+                        console.log("Successfully insertedMany default items into the Item Model Database");
+                    }
+                });
+                foundItems = defaultItems;
+            };
+        });
+    } else {
+        DynamicList.findOne({ dynamicListName: listName }, async function (err, foundList) {
+            if (!foundList) {
+                const newList = new DynamicList({
+                    dynamicListName: listName,
+                    items: defaultItems
+                });
+
+                newList.save(function () {
+                    console.log("docs were saved!");
+                });
+            }
+        })
+    }
+}
+
 // HOME ROUTE
 app.get('/', (req, res) => {
-    Item.find({}, function (err, foundItems) {
-        if (err) { console.log(err); }
-        else if (foundItems.length === 0) {
-            Item.insertMany(defaultItems, function (err) {
-                if (err) { console.log(err); }
-                else { console.log("Successfully insertedMany default items into the Item Model Database"); }
+    Item.find({}, async function (err, foundItems) {
+        if (foundItems.length === 0) {
+            await addDocs(listName);
+            res.redirect("/");
+        } else {
+            res.render('index', {
+                ejsListType: "General To-Do",
+                ejsTodaysDate: dates.todaysDate(),
+                ejsWeekend: dates.weekend(),
+                ejsToDoList: foundItems,
+                ejsDeleteRoute: "/delete",
             });
-            foundItems = defaultItems;
-        };
-
-        res.render('index', {
-            ejsListType: "Regular To-Do-List",
-            ejsTodaysDate: dates.todaysDate(),
-            ejsWeekend: dates.weekend(),
-            ejsToDoList: foundItems,
-            ejsDeleteRoute: "/delete",
-            ejsRoute: "/"
-        });
+        }
     });
 });
+
+// Dynamic Route
+app.get("/:listName", (req, res) => {
+    const urlDynamicList = req.params.listName;
+
+    DynamicList.findOne({ dynamicListName: urlDynamicList }, function (err, foundList) {
+        if (!foundList) {
+
+            async function init() {
+                await addDocs(urlDynamicList);
+                res.redirect("/" + urlDynamicList);
+                console.log("Redirecting");
+            }
+
+            function addDocs(urlDynamicList) {
+                return new Promise((resolve, reject) => {
+                    createDocs(urlDynamicList);
+                    const error = false;
+
+                    if (!error) {
+                        resolve("Finished Creating docs?");
+                    } else {
+                        reject("Somethign went wrong");
+                    }
+                });
+            }
+
+            init();
+
+        } else {
+            function capitalizeFirstLetter(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1);
+            };
+            console.log("Already loaded this page previously");
+            res.render("index", {
+                ejsListType: capitalizeFirstLetter(urlDynamicList),
+                ejsWeekend: dates.weekend(),
+                ejsTodaysDate: dates.todaysDate(),
+                ejsToDoList: foundList.items,
+                ejsDeleteRoute: "/delete/" + urlDynamicList,
+            });
+        }
+
+        if (!foundList) {
+            // createDocs(urlDynamicList);
+            // res.redirect("/" + urlDynamicList);
+        }
+        else {
+            // function capitalizeFirstLetter(string) {
+            //     return string.charAt(0).toUpperCase() + string.slice(1);
+            // };
+
+            // res.render("index", {
+            //     ejsListType: capitalizeFirstLetter(urlDynamicList),
+            //     ejsWeekend: dates.weekend(),
+            //     ejsTodaysDate: dates.todaysDate(),
+            //     ejsToDoList: foundList.items,
+            //     ejsDeleteRoute: "/delete/" + urlDynamicList,
+            // });
+        }
+    });
+});
+
 // ADDING TO DB
 app.post('/', (req, res) => {
-
     const newItem = new Item({
         name: req.body.newItem
     });
 
-    newItem.save(function () {
-        console.log("Added " + newItem + " to the collection");
-        res.redirect('/');
-    });
+    const listName = req.body.dynamicListName.toLowerCase();
+
+    if (listName === "general to-do") {
+        newItem.save(function () {
+            res.redirect('/');
+        });
+    } else {
+        DynamicList.findOne({ dynamicListName: listName }, function (err, foundList) {
+            if (err) {
+                console.log(err)
+            } else if (foundList) {
+                foundList.items.push(newItem);
+                foundList.save(function () {
+                    res.redirect("/" + listName);
+                });
+            }
+        })
+    }
 });
 
 // DELETING FROM DB
 app.post("/delete", (req, res) => {
     checkedItem = req.body.checkedItem;
+    const dynamicListName = req.body.dynamicListName.toLowerCase();
 
-    Item.findByIdAndRemove(checkedItem, function (err, foundItems) {
-        if (err) { console.log(err); }
-        else {
-            console.log(checkedItem);
-            console.log("Successfully deleted " + checkedItem);
-            res.redirect("/");
-        }
-    });
+    if (dynamicListName === "general to-do") {
+        console.log("This is home Page, see dynamicListName: " + dynamicListName);
+        Item.findByIdAndRemove(checkedItem, function (err) {
+            if (err) { console.log(err); }
+            else {
+                console.log("Successfully deleted " + checkedItem);
+                res.redirect("/");
+            }
+        });
+    } else {
+        console.log("This is dynamic Page, see dynamicListName: " + dynamicListName);
+        DynamicList.findOne({ dynamicListName: dynamicListName }, function (err, foundList) {
+            if (err) { console.log(err); }
+            else if (foundList) {
+                console.log("foundList.items b4 pop" + foundList.items);
+                foundList.items.pop();
+                console.log("foundList.items after pop" + foundList.items);
+
+                DynamicList.updateOne({}, { $pull: { _id: checkedItem } }, function (err) {
+                    console.log("Successfully deleted dynamic checkedItem's ID with $pull: " + checkedItem);
+                    res.redirect("/" + dynamicListName);
+                });
+            } else { console.log("Can't find list?"); }
+        });
+    }
 });
 
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-};
 
-function createDynamicModel(DynamicListModel, dynamicListCollection) {
-    DynamicListModel = mongoose.model(dynamicListCollection, toDoListSchema);
-
-    // Create documents
-    const firstItem = new DynamicListModel({
-        name: dynamicListCollection
-    });
-    const secondItem = new DynamicListModel({
-        name: "Second " + dynamicListCollection + " item"
-    });
-
-    const defaultDynamicItems = [firstItem, secondItem];
-
-    addDynamicItems(DynamicListModel, defaultDynamicItems);
-
-}
-
-function addDynamicItems(DynamicListModel, defaultDynamicItems, urlDynamicList) {
-    DynamicListModel.find({}, function (err, foundItems) {
-        if (err) { console.log(err); }
-        else if (foundItems.length === 0) {
-            DynamicListModel.insertMany(defaultDynamicItems, function (err) {
-                if (err) { console.log(err); }
-                else { console.log("Successfully inserted defaultDynamicItems " + defaultDynamicItems); }
-            });
-            foundItems = defaultDynamicItems;
-        }
-    });
-
-    loadDynamicPage(DynamicListModel, urlDynamicList);
-
-}
-
-function loadDynamicPage(DynamicListModel, urlDynamicList) {
-    DynamicListModel.find({}, function (err, foundItems) {
-        if (err) { console.log(err); }
-        else {
-            console.log("found the items Properly");
-            console.log(foundItems);
-            // res.render("index", {
-            //     ejsListType: DynamicListModel + " List",
-            //     ejsWeekend: dates.weekend(),
-            //     ejsTodaysDate: dates.todaysDate(),
-            //     ejsToDoList: foundItems,
-            // ejsDeleteRoute: "/:" + urlDynamicList,
-            // ejsRoute: "/" + urlDynamicList
-            // })
-        }
-    })
-    res.redirect("/");
-}
-
-app.get("/:listName", (req, res) => {
-    const urlDynamicList = req.params.listName;
-    const DynamicListModel = capitalizeFirstLetter(urlDynamicList);
-    const dynamicListCollection = capitalizeFirstLetter(urlDynamicList);
-
-    createDynamicModel(DynamicListModel, dynamicListCollection, urlDynamicList);
-});
-
-app.post("/:listName", (req, res) => {
+app.post("/delete/:listName", (req, res) => {
+    urlDynamicList = req.params.listName;
     checkedItem = req.body.checkedItem;
-    //same DynamicList??
-    DynamicList.findByIdAndRemove(checkedItem, function (err) {
+
+    DynamicList.findByIdAndRemove(checkedItem, function (err, foundItems) {
         if (err) { console.log(err); }
         else {
-            console.log(checkedItem);
+            console.log("checkedItem: " + checkedItem);
             console.log("Successfully deleted " + checkedItem);
-            res.redirect("/" + dynamicList);
-            // Looks fine here too.
+
+            console.log(foundItems);
+
+            res.redirect("/" + urlDynamicList);
         }
     });
-})
-
-app.get('/work', (req, res) => {
-    res.render('index', {
-        ejsListType: "Work To-Do-List!",
-        ejsWeekend: dates.weekend(),
-        ejsTodaysDate: dates.todaysDate(),
-        ejsToDoList: workToDolist,
-        ejsRoute: "/work"
-    });
-})
-
-app.post('/work', (req, res) => {
-    workToDolist.push(req.body.newItem);
-    res.redirect('/work');
 })
 
 app.listen(port, () => {
