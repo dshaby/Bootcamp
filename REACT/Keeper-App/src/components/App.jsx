40222147592b41125fd5d5aac9ad3e0f7096acd0
dshaby <88353394+dshaby@users.noreply.github.com>
2022-04-10 17:28:13 -0700

    Keeper App Part II

--- REACT/Keeper-App/src/components/App.jsx
+++ REACT/Keeper-App/src/components/App.jsx
@@ -1,13 +1,22 @@
 import {Header} from "./Header";
 import { Footer } from "./Footer";
 import { Note } from "./Note";
+import {notes} from "../notes"
+
+function createNote (note) {
+   return(
+   <Note
+   key={note.key}
+   title={note.title}
+   content={note.content} />
+)};
 
 export function App(){
    return(
   <div>
     <Header/>
-   <Note/>
+   {notes.map(createNote)}
    <Footer/>
   </div>
    );
-}
+} 
--- REACT/my-app/src/components/Note.jsx
+++
@@ -1,8 +0,0 @@
-export function Note(){
-    return(
-        <div className="note">
-            <h1>Note title</h1>
-            <p>Note Content</p>
-        </div>
-    );
-}
